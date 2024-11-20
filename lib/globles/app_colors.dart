f2fa8621e1a09aed554b33539103813da2659f39
Shabbir Rajput <sabbirrajput03@gmail.com>
2024-11-20 17:37:52 +0530

    - Changes in App Colors File

--- lib/globles/app_colors.dart
+++ lib/globles/app_colors.dart
@@ -11,3 +11,31 @@ class AppColors {
   static const Color robinEdgeBlue = Color(0xff00CCCC);
   static const Color white = Color(0xffffffff);
 }
+
+/// Commented: New Theme Colors
+/*class AppColors {
+  AppColors._();
+
+  /// Background Colors
+  static const Color bgColor = Color(0xff1E1E2C);
+  static const Color bgColor2 = Color(0xff252A34);
+
+  /// Primary Theme Colors
+  static const Color themeColor = Color(0xffE94560);
+  static const Color themeColorAlt = Color(0xff0F3460);
+
+  /// Accent Colors
+  static const Color aqua = Color(0xff16A085);
+  static const Color robinEdgeBlue = Color(0xff4AA1D4);
+  static const Color vibrantYellow = Color(0xffF9D923);
+
+  /// Secondary Colors
+  static const Color lawGreen = Color(0xff6ECB63);
+  static const Color softOrange = Color(0xffFFA62B);
+  static const Color lavender = Color(0xff9A7AA0);
+
+  /// Neutral Colors
+  static const Color white = Color(0xffffffff);
+  static const Color lightGrey = Color(0xffD3D3D3);
+  static const Color darkGrey = Color(0xff2E2E2E);
+}*/
